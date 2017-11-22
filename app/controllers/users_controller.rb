dbe8cf65057593381bd931205d96e9fda2d27741
Patrick Mollet <mail@patrickmollet.ch>
2017-11-22 09:33:19 +0100

    Users: skip_before_action :authorize

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@
 #---
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authorize
 
   # GET /users
   # GET /users.json
